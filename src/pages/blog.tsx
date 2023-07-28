8cfae8a82af05ca30cb1f7c19737c87a09a0957a
aristata <aristatait@gmail.com>
2023-07-28 16:16:18 +0900

    feat: gatsby plugin 사용하기
    - source-filesystem 설치 및 설정
    - blog 파일에서 graphQL 로 파일 이름 불러오기

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,22 +1,33 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
+import { PageProps, graphql } from "gatsby";
 
 /*************************************************************************************************
  * gatsby 에서의 라우팅
  *
  * - src/pages/ 경로 아래에 JSX 파일이 위치하면 파일명이 라우팅 이름이 된다
  *************************************************************************************************/
-const blog = () => {
+export default function blog({ data }: PageProps<Queries.BlogTitlesQuery>) {
   return (
     <Layout title="Blog">
-      <div>
-        <h1>Blog page</h1>
-      </div>
+      <ul>
+        {data.allFile.nodes.map((file, index) => (
+          <li key={index}>{file.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
-};
+}
 
-export default blog;
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
