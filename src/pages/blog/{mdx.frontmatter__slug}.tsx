9d77f267c3c169c56be260816acdb4bdff6fe952
aristata <aristatait@gmail.com>
2023-07-31 14:16:31 +0900

    feat: BlogPost 내용 만들기

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,35 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { graphql } from "gatsby";
+import Seo from "../../components/Seo";
 
-export default function BlogPost() {
+interface BlogPostProps {
+  data: Queries.PostDetailQuery;
+  children: React.ReactNode;
+}
+export default function BlogPost({ data, children }: BlogPostProps) {
   return (
-    <Layout title="Blog Post">
-      <div />
+    <Layout title={data.mdx?.frontmatter?.title ?? "Blog Post"}>
+      <div>{children}</div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      body
+      frontmatter {
+        category
+        date(formatString: "YYYY.MM.DD")
+        slug
+        title
+      }
+      excerpt(pruneLength: 10)
+    }
+  }
+`;
+
+export const Head = ({ data }: BlogPostProps) => (
+  <Seo title={data.mdx?.frontmatter?.title ?? "Blog Post"} />
+);
