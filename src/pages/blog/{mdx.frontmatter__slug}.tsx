87871205bac1d5aaf833841958c08e249451adbf
aristata <aristatait@gmail.com>
2023-08-01 14:13:32 +0900

    feat: 동적 이미지를 포스트에 추가하기

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -2,15 +2,25 @@ import React from "react";
 import Layout from "../../components/Layout";
 import { graphql } from "gatsby";
 import Seo from "../../components/Seo";
+import { GatsbyImage, getImage, getImageData } from "gatsby-plugin-image";
 
 interface BlogPostProps {
   data: Queries.PostDetailQuery;
   children: React.ReactNode;
 }
 export default function BlogPost({ data, children }: BlogPostProps) {
+  const headerImage = getImage(
+    data.mdx?.frontmatter?.headerImage?.childImageSharp?.gatsbyImageData!!
+  );
   return (
     <Layout title={data.mdx?.frontmatter?.title ?? "Blog Post"}>
-      <div>{children}</div>
+      <>
+        <GatsbyImage
+          image={headerImage!!}
+          alt={data.mdx?.frontmatter?.title ?? "blog post header image"}
+        ></GatsbyImage>
+        <div>{children}</div>
+      </>
     </Layout>
   );
 }
@@ -24,6 +34,11 @@ export const query = graphql`
         date(formatString: "YYYY.MM.DD")
         slug
         title
+        headerImage {
+          childImageSharp {
+            gatsbyImageData(height: 400, placeholder: DOMINANT_COLOR)
+          }
+        }
       }
       excerpt(pruneLength: 10)
     }
