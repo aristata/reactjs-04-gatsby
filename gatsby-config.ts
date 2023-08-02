d5da79c50f5a0ef162df0bb0a75f86a36cd18674
aristata <aristatait@gmail.com>
2023-08-02 14:44:47 +0900

    chore: gatsby config 수정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -26,7 +26,7 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `w1a2xpn7rj24`,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
